6377f3399849f833346195bc4c433871187dc42e
Shyla <shailajasrinivas91@gmail.com>
2023-01-10 15:10:54 -0500

    cart updated

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,21 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Products from "./Products";
 import "./Layout.css";
+import CartItems from "./CartItems";
 const Layout = () => {
-  let total = 100;
+	let total = 100;
 
-  return (
-    <React.Fragment>
-      <div className="layout">
-        <Header />
-        <Products />
-        <div className="total-price">
-          <h3>Total: ${total}</h3>
-          <button className="orderBtn">Place Order</button>
-        </div>{" "}
-      </div>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<div className="layout">
+				<Header />
+				<Products />
+				<CartItems />
+				<div className="total-price">
+					<h3>Total: ${total}</h3>
+					<button className="orderBtn">Place Order</button>
+				</div>{" "}
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Layout;
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth-slice";
+import cartSlice from "./cart-slice";
 
 export const store = configureStore({
 	reducer: {
 		auth: authSlice.reducer,
+		cart: cartSlice.reducer,
 	},
 });
