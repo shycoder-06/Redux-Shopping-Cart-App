fb15a33c9fbac4bd0d63f0e77d2b52b2e05c6352
Shyla <shailajasrinivas91@gmail.com>
2023-01-10 15:52:02 -0500

    thunk pattern  & gethttp request

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
 	name: "cart",
@@ -6,14 +7,21 @@ const cartSlice = createSlice({
 		itemsList: [],
 		totalQuantity: 0,
 		showCart: false,
+		changed: false,
 	},
 	reducers: {
+		replaceData(state, action) {
+			state.totalQuantity = action.payload.totalQuantity;
+			state.itemsList = action.payload.itemsList;
+		},
 		addToCart(state, action) {
+			state.changed = true;
 			const newItem = action.payload;
-			//to check if item is already available
+			// to check if item is already available
 			const existingItem = state.itemsList.find(
 				(item) => item.id === newItem.id
 			);
+
 			if (existingItem) {
 				existingItem.quantity++;
 				existingItem.totalPrice += newItem.price;
@@ -28,11 +36,25 @@ const cartSlice = createSlice({
 				state.totalQuantity++;
 			}
 		},
-		removeFromCart() {},
+		removeFromCart(state, action) {
+			state.changed = true;
+			const id = action.payload;
+
+			const existingItem = state.itemsList.find((item) => item.id === id);
+			if (existingItem.quantity === 1) {
+				state.itemsList = state.itemsList.filter((item) => item.id !== id);
+				state.totalQuantity--;
+			} else {
+				existingItem.quantity--;
+				existingItem.totalPrice -= existingItem.price;
+			}
+		},
 		setShowCart(state) {
 			state.showCart = !state.showCart;
 		},
 	},
 });
+
 export const cartActions = cartSlice.actions;
+
 export default cartSlice;
